2906254c9087416ec17d0f642892e4af7b82f62c
Vlad Iacob <vlad.iacob@hackajob.co>
2021-09-10 14:17:03 +0300

    .theia config for a better intentation

--- src/FirstComponent.js
+++ src/FirstComponent.js
@@ -2,18 +2,19 @@ import React from 'react';
 import './static/style/main.css';
 
 class FirstComponent extends React.Component {
- render() {
-  return(
-	<>
-		<div id="article">
-		    <h1>Good Luck!</h1>
-		    <div>
-		        <p>“Java is to JavaScript what Car is to Carpet.”</p>
-		        <p>— Chris Heilmann</p>
-		    </div>
-	    	</div>
-	</>
-  );
- }
+  render() {
+    return (
+      <>
+        <div id="article">
+          <h1>Good Luck!</h1>
+          <div>
+            <p>“Java is to JavaScript what Car is to Carpet.”</p>
+            <p>— Chris Heilmann</p>
+          </div>
+        </div>
+      </>
+    );
+  }
 }
+
 export default FirstComponent
